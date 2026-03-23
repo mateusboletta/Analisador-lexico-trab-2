4c800f60f4cdd6e74ce718ca14b964f4a6d785b5
Mateus Boletta <117239443+mateusboletta@users.noreply.github.com>
2026-03-23 16:15:31 -0300

    Após adições de mais tokens

--- tokens.h
+++ tokens.h
@@ -3,3 +3,16 @@
 #define INT 258
 #define GE 259
 #define IF 260
+//ADD
+#define FOR 261
+#define WHILE 262
+#define CHAR 263
+#define VOID 264
+#define FLOAT 265
+#define LE 266
+#define EQ 267
+#define NE 268
+#define AND 269
+#define OR 270
+#define NOT 271
+#define STRING 272
